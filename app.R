f2b14e25cce41f61a6f22567567b9b4acd20a87a
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2025-03-08 18:44:48 +0800

    Update code with details button, and re-arranging the sqlite table call.

--- app.R
+++ app.R
@@ -16,15 +16,14 @@ library(DBI)
 library(bslib)
 library(RSQLite)
 library(reactable)
+library(htmltools)
 # renv::install("reactable")
 # renv::install("RSQLite")
 # renv::install("tippy")
 
 kahler <- DBI::dbConnect(RSQLite::SQLite(), "kahler.sqlite")
 
-k_full <- tbl(kahler, "full")
-
-k_stem <- k_full |> 
+k_stem <- tbl(kahler, "full") |> 
   select(matches("^stem|kms_page|kms_entry|kms_Alphabet"))
 
 k_stem_interim <- k_stem |> 
@@ -37,23 +36,27 @@ k_stem_interim <- k_stem |>
          German = stem_DE, 
          English = stem_EN, 
          Indonesian = stem_IDN) |> 
-  mutate(form = if_else(!is.na(stem_homonymID),
-                        paste(form, 
-                              "<sup><i>", 
-                              stem_homonymID, 
-                              "</i></sup>", 
-                              sep = ""),
-                        form),
-         entry = toupper(entry)) |> 
+  mutate(# form = if_else(!is.na(stem_homonymID),
+                        # paste(form, 
+                        #       "<sup><i>", 
+                        #       stem_homonymID, 
+                        #       "</i></sup>", 
+                        #       sep = ""),
+                        # form),
+         # form = str_c("<a href=#", stem_id, ">", form, "</a>",
+         #                      sep = ""),
+         entry = toupper(entry),
+         details = NA) |> 
+  relocate(details, .before = form) |> 
   distinct() |> 
   filter(!is.na(German)) |>
-  select(-stem_homonymID) |> 
+  # select(-stem_homonymID) |> 
   collect() |> 
   mutate(across(where(is.character), ~replace_na(., "")))
 
 subentry_cols <- "^ex|stem_form|stem_homonymID$|stem_ID|stem_(DE|EN|IDN)|kms_page|kms_Alphabet"
 
-k_subentry <- k_full |> 
+k_subentry <- tbl(kahler, "full") |> 
   select(matches(subentry_cols)) |> 
   filter(!is.na(example_id))
 
@@ -71,17 +74,17 @@ k_subentry_interim <- k_subentry |>
          German = stem_DE, 
          English = stem_EN, 
          Indonesian = stem_IDN) |> 
-  mutate(`main entry` = if_else(!is.na(stem_homonymID),
-                                paste(`main entry`, 
-                                      "<sup><i>", 
-                                      stem_homonymID, 
-                                      "</i></sup>", 
-                                      sep = ""),
-                                `main entry`),
+  mutate(#`main entry` = if_else(!is.na(stem_homonymID),
+                                # paste(`main entry`, 
+                                #       "<sup><i>", 
+                                #       stem_homonymID, 
+                                #       "</i></sup>", 
+                                #       sep = ""),
+                                # `main entry`),
          entry = toupper(entry)) |> 
   distinct() |> 
   # filter(!is.na(German)) |> 
-  select(-stem_homonymID) |> 
+  # select(-stem_homonymID) |>
   collect() |> 
   mutate(across(where(is.character), ~replace_na(., "")))
 
@@ -173,14 +176,15 @@ ui <- page_navbar(
   theme = bs_theme(
     version = bslib::version_default(),
     bootswatch = "cosmo",
-    bg = "#f9f8f5", 
+    bg = "#f9f8f5",
     fg = "#002147", 
     primary = "#193658",
     # primary = "#1D42A6",
     # secondary="#003947",
     secondary = "#E4F0EF",
     "link-hover-color" = "#be0f34",
-    "link-color" = "#3277ae"
+    "link-color" = "#3277ae",
+    info = "#00AAB4"
     # source from Oxford colour parameters: https://communications.web.ox.ac.uk/communications-resources/visual-identity/identity-guidelines/colours
   ),
   # collapsible = TRUE,
@@ -195,7 +199,7 @@ ui <- page_navbar(
                 HTML('<p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://doi.org/10.25446/oxford.28057742">The Enggano-German Dictionary online derived from Kähler’s (1987) “Enggano-Deutsches Wörterbuch”</a> by <span property="cc:attributionName">Gede Primahadi Wijaya Rajeg, Cokorda Rai Adi Pramartha, Ida Bagus Gede Sarasvananda, Putu Wahyu Widiatmika, Ida Bagus Made Ari Segara, Yul Fulgensia Rusman Pita, Fitri Koemba, I Gede Semara Dharma Putra, Putu Dea Indah Kartini, Ni Putu Wulan Lestari, Barnaby Burleigh, Charlotte Hemmings, I Wayan Arka, Sarah Ogilvie, Mary Dalrymple, Daniel Krauße, and Bernd Nothofer</span> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></a></p>'),
                 h2("Overview"),
                 div(p("Welcome to the ", a("Shiny", href = "https://shiny.posit.co/", target = "_blank"), "web application serving the selected content from the retro-digitised Enggano-German dictionary dataset ", a("(Rajeg et al. 2024)", href = "https://doi.org/10.25446/oxford.28057742.v1", target = "_blank"), "by ", a("Hans Kähler (1987).", href = "https://search.worldcat.org/title/18191699", target = "_blank"))),
-                div(p("At the moment, users can browse (i) the ", actionLink("headword", "main entry"), " represented in the original dictionary or (ii) the ", actionLink("subentry", "sub-entry"), " (if any) for a given main entry (i.e., a headword/a root form). In the ", strong("Sub-entry"), "panel, we also provide information about the main entry that the sub-entry belongs to (under the", tags$code("main entry"), "column). The German, English, and Indonesian translations for the sub-entries are marked with", tags$code("(sub)"), "in the column names.")),
+                div(p("At the moment, users can browse (i) the ", strong(actionLink("headword", "main entry")), " represented in the original dictionary or (ii) the ", strong(actionLink("subentry", "sub-entry")), " (if any) for a given main entry (i.e., a headword/a root form). In the ", strong("Sub-entry"), "panel, we also provide information about the main entry that the sub-entry belongs to (under the", tags$code("main entry"), "column). The German, English, and Indonesian translations for the sub-entries are marked with", tags$code("(sub)"), "in the column names.")),
                 div(p("The search field for each column accepts plain search pattern and regular expressions; for these two flavours of the search patterns, each match will be highlighted in red.")),
                 div(p("Related materials for this sub-project are available ", a("here", href = "https://portal.sds.ox.ac.uk/projects/Retro-digitisation_of_the_Enggano-German_Dictionary/237998", target = "_blank"), ".")),
                 
@@ -274,56 +278,68 @@ server <- function(input, output, session) {
   k_stem_out <- reactive({
     
     k_stem_interim |> 
-    
-    reactable::reactable(filterable = TRUE,
-                         searchable = TRUE,
-                         searchMethod = regex_search_method,
-                         showPagination = TRUE,
-                         highlight = TRUE,
-                         resizable = TRUE,
-                         # height = 495,
-                         minRows = 5,
-                         defaultPageSize = 10,
-                         onClick = JS(
-                           "function(rowInfo, column) {
-                           if(column.id !== \"entry\")
+      
+      reactable::reactable(filterable = TRUE,
+                           searchable = TRUE,
+                           searchMethod = regex_search_method,
+                           showPagination = TRUE,
+                           highlight = TRUE,
+                           resizable = TRUE,
+                           # height = 495,
+                           minRows = 5,
+                           language = reactableLang(
+                             filterPlaceholder = "Search"
+                           ),
+                           defaultPageSize = 10,
+                           onClick = JS(
+                             "function(rowInfo, column) {
+                           if(column.id === \"details\")
                            Shiny.setInputValue('main_entry_details', { index: rowInfo.index + 1 }, { priority: 'event' })
                            }
                            "
-                         ),
-                         elementId = "alphabet-select",
-                         columns = list(
-                           entry = reactable::colDef(show = TRUE, 
-                                                     maxWidth = 80,
-                                                     filterInput = function(values, name) {
-                                                       tags$select(
-                                                         # Set to undefined to clear the filter
-                                                         onchange = sprintf("Reactable.setFilter('alphabet-select', '%s', event.target.value || undefined)", name),
-                                                         # "All" has an empty value to clear the filter, and is the default option
-                                                         tags$option(value = "", "All"),
-                                                         lapply(unique(values), tags$option),
-                                                         "aria-label" = sprintf("Filter %s", name),
-                                                         style = "width: 100%; height: 28px;"
-                                                       )
-                                                     }),
-                           form = reactable::colDef(html = TRUE, 
-                                                    maxWidth = 185,
-                                                    filterMethod = js_filter,
-                                                    cell = js_match_style),
-                           German = reactable::colDef(html = TRUE,
+                           ),
+                           elementId = "alphabet-select",
+                           columns = list(
+                             details = colDef(
+                               name = "",
+                               sortable = FALSE,
+                               cell = function() htmltools::tags$button("more", class="btn btn-info btn-sm btn-border-radius-lg"),
+                               filterable = FALSE,
+                               maxWidth = 83
+                             ),
+                             
+                             entry = reactable::colDef(show = TRUE, 
+                                                       maxWidth = 70,
+                                                       filterInput = function(values, name) {
+                                                         tags$select(
+                                                           # Set to undefined to clear the filter
+                                                           onchange = sprintf("Reactable.setFilter('alphabet-select', '%s', event.target.value || undefined)", name),
+                                                           # "All" has an empty value to clear the filter, and is the default option
+                                                           tags$option(value = "", "All"),
+                                                           lapply(unique(values), tags$option),
+                                                           "aria-label" = sprintf("Filter %s", name),
+                                                           style = "width: 100%; height: 28px;"
+                                                         )
+                                                       }),
+                             form = reactable::colDef(html = TRUE, 
+                                                      maxWidth = 185,
                                                       filterMethod = js_filter,
                                                       cell = js_match_style),
-                           English = reactable::colDef(html = TRUE, 
-                                                       filterMethod = js_filter,
-                                                       cell = js_match_style),
-                           Indonesian = reactable::colDef(html = TRUE, 
-                                                          filterMethod = js_filter,
-                                                          cell = js_match_style),
-                           stem_id = reactable::colDef(show = FALSE),
-                           page = reactable::colDef(show = FALSE)
-                         )
-    )
-  
+                             German = reactable::colDef(html = TRUE,
+                                                        filterMethod = js_filter,
+                                                        cell = js_match_style),
+                             English = reactable::colDef(html = TRUE, 
+                                                         filterMethod = js_filter,
+                                                         cell = js_match_style),
+                             Indonesian = reactable::colDef(html = TRUE, 
+                                                            filterMethod = js_filter,
+                                                            cell = js_match_style),
+                             stem_id = reactable::colDef(show = FALSE),
+                             stem_homonymID = reactable::colDef(show = FALSE),
+                             page = reactable::colDef(show = FALSE)
+                           )
+      )
+    
   })
   
   k_subentry_out <- reactive({
@@ -338,6 +354,9 @@ server <- function(input, output, session) {
                          highlight = TRUE,
                          # height = 495,
                          minRows = 5,
+                         language = reactableLang(
+                           filterPlaceholder = "Search"
+                         ),
                          defaultPageSize = 10,
                          elementId = "alphabet-select",
                          columns = list(
@@ -380,7 +399,8 @@ server <- function(input, output, session) {
                                                                   cell = js_match_style),
                            stem_id = reactable::colDef(show = FALSE),
                            example_id = reactable::colDef(show = FALSE),
-                           page = reactable::colDef(show = FALSE)
+                           page = reactable::colDef(show = FALSE),
+                           stem_homonymID = reactable::colDef(show = FALSE)
                          )
     )
   
@@ -402,6 +422,10 @@ server <- function(input, output, session) {
       row_num <- as.numeric(input$main_entry_details$index)
       main_entry_id <- k_stem_interim$stem_id[row_num]
       main_entry_form <- k_stem_interim$form[row_num]
+      main_entry_homonym_id <- k_stem_interim$stem_homonymID[row_num]
+      main_entry_form <- if_else(is.na(main_entry_homonym_id), 
+                                 str_c("<i>", main_entry_form, "</i>", sep = ""),
+                                 str_c("<i>", main_entry_form, "<sup>", main_entry_homonym_id, "</sup></i>", sep = ""))
       k_stem_filtered <- filter(k_stem, stem_id == main_entry_id) |> 
         distinct()
       # page number
@@ -429,8 +453,8 @@ server <- function(input, output, session) {
       # remarks info
       stem_remark <- k_stem_filtered |> 
         filter(!is.na(stem_remark_DE)) |> 
-        mutate(myremarks = str_c("<li>DE: ", stem_remark_DE, "</li><li>EN: ", 
-                                 stem_remark_EN, "</li><li>ID: ", 
+        mutate(myremarks = str_c("<li><sub><i>DE</i></sub>", stem_remark_DE, "</li><li><sub><i>EN</i></sub>", 
+                                 stem_remark_EN, "</li><li><sub><i>ID</i></sub>", 
                                  stem_remark_IDN, "</li>", sep = "")) |> 
         pull(myremarks)
       
@@ -440,8 +464,8 @@ server <- function(input, output, session) {
           easyClose = TRUE,
           title = "Details",
           
-          tags$h3(HTML(main_entry_form)),
-          tags$p("DE:", k_stem_interim$German[row_num], HTML("</br>EN:"), k_stem_interim$English[row_num], HTML("</br>ID:"), k_stem_interim$Indonesian[row_num]),
+          tags$h2(HTML(main_entry_form)),
+          tags$p(HTML("<li>"), k_stem_interim$German[row_num], HTML("<sub><i>DE</i></sub></li><li>"), k_stem_interim$English[row_num], HTML("<sub><i>EN</i></sub></li><li>"), k_stem_interim$Indonesian[row_num], HTML("<sub><i>ID</i></sub></li>")),
           if (any(!is.na(variant_forms))) {
             div(p(HTML("<b>variant form(s)</b>:</br><li>", variant_forms, "</li>")))
           },
